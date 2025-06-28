033b566d0e9b4fb020c8d15e870a1cd8897ffa01
Kamil Szpak <dev@kamilszpak.com>
2025-06-28 13:13:02 +0200

    Initiate the 'Defining Data Models' tutorial chapter, providing a foundational guide for building apps with SymbolPicker.

---
+++ Sources/SymbolPicker/SymbolPicker.docc/Resources/Tutorials/defining-data-models/code/DDM-Section3-Step4.swift
@@ -0,0 +1,25 @@
+//
+//  Type.swift
+//  Checker
+//
+//  Created by Kamil Szpak on 27/06/2025.
+//
+
+import Foundation
+import SymbolPicker
+import SwiftData
+
+@Model
+class Type: Identifiable, Equatable, Hashable, Comparable, Hashable{
+    var id = UUID()
+    var title: String
+    var symbolName: String
+    var color: SymbolColor
+    
+    init(title: String = "", details: String = "", symbolName: String = "archivebox", symbolColor: SymbolColor = .purple) {
+        self.title = title
+        self.symbolName = symbolName
+        self.symbolColor = symbolColor
+    }
+    
+}
---
+++ Sources/SymbolPicker/SymbolPicker.docc/Resources/Tutorials/defining-data-models/code/DDM-Section4-Step1.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  Checker
+//
+//  Created by Kamil Szpak on 21/06/2025.
+//
+
+import Foundation
+
