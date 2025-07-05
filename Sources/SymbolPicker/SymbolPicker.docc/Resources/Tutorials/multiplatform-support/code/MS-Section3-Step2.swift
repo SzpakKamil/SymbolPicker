d532b017024f40936ca6fdf25f024db4cd572e4e
Kamil Szpak <dev@kamilszpak.com>
2025-07-05 21:47:05 +0200

    Finish the Multiplatform Support tutorial sections and complete the project-wide documentation suite.

---
+++ Sources/SymbolPicker/SymbolPicker.docc/Resources/Tutorials/multiplatform-support/code/MS-Section3-Step2.swift
@@ -0,0 +1,23 @@
+//
+//  CheckerApp.swift
+//  Checker
+//
+//  Created by Kamil Szpak on 27/06/2025.
+//
+
+import SwiftUI
+import SymbolPicker
+import SwiftData
+
+@main
+struct CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            #if os(macOS)
+                .frame(width: 400, height: 400)
+            #endif
+        }
+        .modelContainer(for: [Event.self, Type.self])
+    }
+}
